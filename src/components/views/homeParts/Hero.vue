6b2e8f28045cd0153bc203fae83aeb9a544fbbc9
Konagi-iz <cmaxwsl@gmail.com>
2024-01-11 20:07:06 +0900

    90%

--- src/components/views/homeParts/Hero.vue
+++ src/components/views/homeParts/Hero.vue
@@ -1,10 +1,10 @@
 <script setup>
 import { Swiper, SwiperSlide } from 'swiper/vue';
-import { Autoplay, EffectFade, Mousewheel, Pagination } from 'swiper/modules';
-const modules = [Autoplay, EffectFade, Mousewheel, Pagination];
+import { Autoplay, EffectCreative, Pagination } from 'swiper/modules';
+const modules = [Autoplay, EffectCreative, Pagination];
 import 'swiper/css';
 import 'swiper/css/autoplay';
-import 'swiper/css/effect-fade';
+import 'swiper/css/effect-creative';
 import 'swiper/css/pagination';
 import { ref } from 'vue';
 
@@ -19,14 +19,12 @@ const onSwiper = (_swiper) => {
 
 const progressCircle = ref(null);
 const onAutoplayTimeLeft = (s, time, progress) => {
-	console.log(progress);
 	progressCircle.value.style.setProperty('--progress', 1 - progress);
 };
 
 const changeSwiperIndex = () => {
 	if (heroSlide.swiper !== undefined) {
-		currentIndex.value = heroSlide.swiper.activeIndex + 1;
-		console.log(currentIndex.value);
+		currentIndex.value = heroSlide.swiper.realIndex + 1;
 	}
 };
 
@@ -56,6 +54,17 @@ const slides = ref([
 		txt: 'UCIが規定する枠内でエアロ性能に徹底的にこだわった。\nその結果、可能な限りの限界に\n挑戦したバイクが誕生した。',
 	},
 ]);
+
+const splitText = (str) => {
+	const result = str.split('');
+	let newText = '';
+	for (let i = 0; i < result.length; i++) {
+		newText += '<span>' + result[i] + '</span>';
+	}
+	return newText;
+};
+
+const navs = ref(['BIKES', 'SHOP LIST', 'ONLINE SHOP', 'SUPPORT', 'HISTORY']);
 </script>
 
 <template>
@@ -64,15 +73,15 @@ const slides = ref([
 			class="lcl-hero-slides"
 			@swiper="onSwiper"
 			@slideChange="changeSwiperIndex"
+			@autoplayTimeLeft="onAutoplayTimeLeft"
 			ref="heroSlide"
 			:modules="modules"
 			:autoplay="{ delay: 6000 }"
-			effect="fade"
+			effect="creative"
 			:loop="true"
 			:loop-additional-slides="1"
-			:mousewheel="{ forceToAxis: true }"
 			:pagination="{ clickable: true }"
-			@onAutoplayTimeLeft="onAutoplayTimeLeft"
+			:speed="300"
 		>
 			<SwiperSlide class="lcl-hero-slides__slide" v-for="(slide, index) in slides" :key="`slide-${index}`">
 				<div class="lcl-hero-slides__in">
@@ -83,7 +92,7 @@ const slides = ref([
 						<p class="lcl-hero-slides__model">{{ slide.model }}</p>
 					</div>
 					<div class="lcl-hero-slides__txts">
-						<p class="lcl-hero-slides__copy">{{ slide.copy }}</p>
+						<p class="lcl-hero-slides__copy" v-html="splitText(slide.copy)"></p>
 						<p class="lcl-hero-slides__txt" style="white-space: pre-wrap" v-text="slide.txt"></p>
 					</div>
 				</div>
@@ -95,11 +104,22 @@ const slides = ref([
 				<span class="lcl-hero-slides__index">{{ currentIndex }}</span>
 			</div>
 		</Swiper>
+		<div class="lcl-hero-foot">
+			<ul class="lcl-hero-foot__nav">
+				<li class="lcl-hero-foot__nav-item" v-for="nav in navs">
+					<a class="lcl-hero-foot__nav-link" :href="`#${nav.replace(' ', '').toLocaleLowerCase()}`">{{ nav }}</a>
+				</li>
+			</ul>
+			<div class="lcl-hero-foot__scroll">
+				
+			</div>
+		</div>
 	</div>
 </template>
 
 <style lang="scss">
 .lcl-hero {
+	position: relative;
 	width: 100%;
 	height: 100vh;
 	background: url('/assets/img/bg/bg_grid.png') repeat left top, $c-lightgray;
@@ -129,7 +149,10 @@ const slides = ref([
 		position: absolute;
 		top: 0;
 		left: 0;
+		// transform: translateX(-30%);
+		opacity: 0;
 		width: pcvw(900);
+		transition: opacity 0.5s $e-out;
 	}
 	.lcl-hero-slides__model-wrp {
 		position: absolute;
@@ -169,9 +192,21 @@ const slides = ref([
 		font-size: vwclamp(32);
 		font-weight: 600;
 		line-height: 1.8;
+		span {
+			display: inline-block;
+			transform: translateX(200%);
+			opacity: 0;
+			transition: transform 0.5s $e-out, opacity 0.5s $e-out;
+			@for $i from 1 through 50 {
+				&:nth-of-type(#{$i}) {
+					transition-delay: calc(0.03s * ($i - 1) + 0.7s);
+				}
+			}
+		}
 	}
 	.lcl-hero-slides__txt {
 		transform: skewX(-10deg);
+		opacity: 0;
 		color: $c-red;
 		font-size: vwclamp(12);
 		font-weight: 600;
@@ -232,6 +267,192 @@ const slides = ref([
 	}
 	.swiper-pagination-bullet-active {
 		opacity: 1;
+	}
+	/* active ------------ */
+	.swiper-slide-active {
+		.lcl-hero-slides__img {
+			transform: translateX(0);
+			opacity: 1;
+			animation: img-active 3s 0.1s $e-out;
+		}
+		.lcl-hero-slides__model {
+			animation: model-active 4s $e-out;
+		}
+		.lcl-hero-slides__copy {
+			span {
+				transform: translateX(0);
+				opacity: 1;
+			}
+		}
+		.lcl-hero-slides__txt {
+			animation: txt-active 4s 1s $e-out forwards;
+		}
+	}
+	@keyframes img-active {
+		0% {
+			transform: translate(-50%, 0);
+			opacity: 1;
+		}
+		1% {
+			opacity: 0;
+		}
+		2% {
+			opacity: 1;
+		}
+		3% {
+			opacity: 0;
+		}
+		4% {
+			opacity: 1;
+		}
+		5% {
+			opacity: 0;
+		}
+		19% {
+			transform: translate(0, 0);
+		}
+		20% {
+			transform: translate(0, 0);
+			opacity: 1;
+		}
+		21% {
+			transform: translate(0, 0);
+			opacity: 0;
+		}
+		22% {
+			transform: translate(0, 0);
+			opacity: 1;
+		}
+		23% {
+			transform: translate(0, 0);
+			opacity: 0;
+		}
+		24% {
+			transform: translate(0, 0);
+			opacity: 1;
+		}
+		25% {
+			transform: translate(0, 0);
+			opacity: 1;
+		}
+		100% {
+			transform: translate(0, 0);
+			opacity: 1;
+		}
+	}
+	@keyframes model-active {
+		0% {
+			transform: scale(5) translate(-50%, 20%);
+			opacity: 1;
+		}
+		1% {
+			opacity: 0;
+		}
+		2% {
+			opacity: 1;
+		}
+		3% {
+			opacity: 0;
+		}
+		4% {
+			opacity: 1;
+		}
+		5% {
+			opacity: 0;
+		}
+		19% {
+			transform: scale(5) translate(0, 20%);
+		}
+		20% {
+			transform: scale(5) translate(0, 0);
+			opacity: 1;
+		}
+		21% {
+			transform: scale(5) translate(0, 0);
+			opacity: 0;
+		}
+		22% {
+			transform: scale(5) translate(0, 0);
+			opacity: 1;
+		}
+		23% {
+			transform: scale(5) translate(0, 0);
+			opacity: 0;
+		}
+		24% {
+			transform: scale(5) translate(0, 0);
+			opacity: 1;
+		}
+		25% {
+			transform: scale(1) translate(0, 0);
+			opacity: 1;
+		}
+		100% {
+			transform: scale(1) translate(0, 0);
+			opacity: 1;
+		}
+	}
+	@keyframes txt-active {
+		0% {
+			opacity: 0;
+		}
+		1% {
+			opacity: 1;
+		}
+		2% {
+			opacity: 0;
+		}
+		3% {
+			opacity: 1;
+		}
+		4% {
+			opacity: 0;
+		}
+		5% {
+			opacity: 1;
+		}
+		6% {
+			opacity: 0;
+		}
+		7% {
+			opacity: 1;
+		}
+		100% {
+			opacity: 1;
+		}
+	}
+	/* prev ------------ */
+	.swiper-slide-prev {
+		.lcl-hero-slides__img {
+			transform: translateX(30%);
+		}
+	}
+	/* foot ------------ */
+	.lcl-hero-foot {
+		position: absolute;
+		bottom: 0;
+		left: 0;
+		display: flex;
+		width: 100%;
+		height: vwclamp(64);
+	}
+	.lcl-hero-foot__nav {
+		display: flex;
+		align-items: center;
+		justify-content: space-between;
+		padding-inline: vwclamp(73);
+		width: vwclamp(689);
+		height: 100%;
+	}
+	.lcl-hero-foot__nav-item {
+	}
+	.lcl-hero-foot__nav-link {
+		color: $c-darkgray;
+		@include font-en;
+		@include fz(10);
+		font-weight: 300;
+		line-height: 1.2;
+		font-style: italic;
 	}
 }
 </style>
